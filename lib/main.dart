46114afaba8a5a902d36d8bdfe85c95f28d39eea
Emanuelle Passos <62838314+Emanuelle-p@users.noreply.github.com>
2023-08-22 22:01:25 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,10 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        HOME);
+        routes: {
+            AppRoutes.HOME: (_) => MeusVeiculosList()
+            AppRoutes.VEICULO_FORM: (_) => VeiculoForm()
+        },
   }
 }
 
